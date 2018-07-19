ce0e1cf403db0d199a05c0fcfdc6b6b79754e42f
Evan Varan <evan.varan@gmail.com>
2018-07-19 19:35:45 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/Unity/Evan/teststststs@6 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ Assets/DungeonCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DungeonCamera : MonoBehaviour {
+    public Transform target;
+    public float smoothTime = .2f;
+    Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 goalPos = target.position;
+        goalPos.y = transform.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+    }
+}
+
---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float movementSpeed = 10;
+    public float turningSpeed = 600;
+
+    // Use this for initialization
+    void Start () {
+        print("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+
+    }
+}
